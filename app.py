73a779a0df9b843e7100b437e91333d210202116
jahnavireddyn <116332021+jahnavireddyn@users.noreply.github.com>
2024-04-26 10:14:43 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ObK6R-4UXt2TvhI7uPikIlo1drKkvHQn
 """
 
-!pip install streamlit
+pip install streamlit
 
 #importing libraries
 import streamlit as st
