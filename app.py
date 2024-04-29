5bc3c70f1f9ecb36b13552bd4010dbb7f4f2d43d
jahnavireddyn <116332021+jahnavireddyn@users.noreply.github.com>
2024-04-29 10:01:38 +0530

    app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ObK6R-4UXt2TvhI7uPikIlo1drKkvHQn
 """
 
-pip install opencv
-
+pip install opencv-python
 #importing libraries
 import streamlit as st
 import os
