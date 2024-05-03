8abd94b011bd8fe6f304ce9deb6918a4239d0de7
jahnavireddyn <116332021+jahnavireddyn@users.noreply.github.com>
2024-05-03 10:29:31 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -73,7 +73,7 @@ def get_binary_file_downloader_html(file_path, title="Download File"):
 
 def main():
     st.title("Human Activity Recognition - Fight Detection")
-    st.write("Upload a video to detect as 'fight' or no-fight'")
+    st.header("Upload a video to detect as 'fight' or no-fight'")
 
     uploaded_file = st.file_uploader("Upload a video", type=['mp4'])
     if uploaded_file is not None:
